d59616fa3e8e655b64a0360cfec45dc2419349df
Tosuke <tasukeprg@gmail.com>
2019-06-01 16:53:54 +0900

    feat: add 'boolean'

--- src/primitives.ts
+++ src/primitives.ts
@@ -1,16 +1,20 @@
-import { BiTransformer } from './transformer'
+import { Transformer } from './transformer'
 import { ok, error } from './result'
-import { ValidationTypeError } from './error'
+import { ValidationTypeError } from './errors'
 
-export const any = new BiTransformer<unknown, unknown>(
+export const any = new Transformer<unknown, unknown>(
   u => (u != null ? ok(u) : error(new ValidationTypeError('any', typeof u))),
   ok,
 )
-export const number = new BiTransformer<unknown, number>(
-  u => typeof u === 'number' ? ok(u) : error(new ValidationTypeError('number', typeof u)),
+export const number = new Transformer<unknown, number>(
+  u => (typeof u === 'number' ? ok(u) : error(new ValidationTypeError('number', typeof u))),
   ok,
 )
-export const string = new BiTransformer<unknown, string>(
-  u => typeof u === 'string' ? ok(u) : error(new ValidationTypeError('string', typeof u)),
+export const string = new Transformer<unknown, string>(
+  u => (typeof u === 'string' ? ok(u) : error(new ValidationTypeError('string', typeof u))),
+  ok,
+)
+export const boolean = new Transformer<unknown, boolean>(
+  u => (typeof u === 'boolean' ? ok(u) : error(new ValidationTypeError('boolean', typeof u))),
   ok,
 )

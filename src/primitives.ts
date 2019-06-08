db2b06ec53e071174f79e4945b4015ccc6d1e335
Tosuke <tasukeprg@gmail.com>
2019-06-08 19:48:36 +0900

    feat: add `typeOf` transformer

--- src/primitives.ts
+++ src/primitives.ts
@@ -3,18 +3,31 @@ import { ok, error } from './result'
 import { ValidationTypeError } from './errors'
 import { toTypeName } from './util'
 
+const transformerCache = new Map<unknown, Transformer<unknown, any>>()
+
+export function typeOf(type: 'string'): Transformer<unknown, string>
+export function typeOf(type: 'number'): Transformer<unknown, number>
+export function typeOf(type: 'bigint'): Transformer<unknown, bigint>
+export function typeOf(type: 'boolean'): Transformer<unknown, boolean>
+export function typeOf(type: 'symbol'): Transformer<unknown, symbol>
+export function typeOf(type: 'undefined'): Transformer<unknown, undefined>
+export function typeOf(type: 'object'): Transformer<unknown, object>
+export function typeOf(type: 'function'): Transformer<unknown, Function>
+export function typeOf(
+  type: 'string' | 'number' | 'bigint' | 'boolean' | 'symbol' | 'undefined' | 'object' | 'function',
+): Transformer<unknown, any> {
+  if (transformerCache.has(type)) return transformerCache.get(type)!
+  const transform = (u: unknown) =>
+    typeof u === type ? ok(u) : error(new ValidationTypeError([], type, toTypeName(u)))
+  const transformer = new Transformer<unknown, any>(transform, transform)
+  transformerCache.set(type, transformer)
+  return transformer
+}
+
 const noNullOrUndefined = (u: unknown) => (u != null ? ok(u) : error(new ValidationTypeError([], 'any', toTypeName(u))))
 
 export const any = new Transformer<unknown, unknown>(noNullOrUndefined, noNullOrUndefined)
-export const number = new Transformer<unknown, number>(
-  u => (typeof u === 'number' ? ok(u) : error(new ValidationTypeError([], 'number', toTypeName(u)))),
-  ok,
-)
-export const string = new Transformer<unknown, string>(
-  u => (typeof u === 'string' ? ok(u) : error(new ValidationTypeError([], 'string', toTypeName(u)))),
-  ok,
-)
-export const boolean = new Transformer<unknown, boolean>(
-  u => (typeof u === 'boolean' ? ok(u) : error(new ValidationTypeError([], 'boolean', toTypeName(u)))),
-  ok,
-)
+
+export const number = typeOf('number')
+export const string = typeOf('string')
+export const boolean = typeOf('boolean')

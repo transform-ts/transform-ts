29ee66668571278deba68bb73f6594df0e8b0a09
Tosuke <tasukeprg@gmail.com>
2019-08-16 13:58:15 +0900

    chore: change export strategy

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
 export { Transformer } from './transformer'
 export { ValidationError, ValidationTypeError, ValidationMemberError, ValidationErrors } from './errors'
 export { Result, ValidationResult, ok, error, isOk, isError } from './result'
-export { any, number, string, boolean } from './primitives'
-export { nullable, optional, array, tuple, obj } from './combinators'
+
+import { any, number, string, boolean, literal } from './primitives'
+import { nullable, optional, array, tuple, obj, either } from './combinators'
+
+export default {
+  any,
+  number,
+  string,
+  boolean,
+  literal,
+  nullable,
+  optional,
+  array,
+  tuple,
+  obj,
+  either,
+}
